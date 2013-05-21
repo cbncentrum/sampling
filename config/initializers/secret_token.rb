9decd44b4e7142f59db3edaf2103cf9b21102c3a
cbncentrum <cbncentrum@gmail.com>
2013-05-21 22:50:50 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sampling::Application.config.secret_token = 'ca4f19e100af2560902b6d42f6635fa3d629ddc9ddc9c093cc5450915f632b8c351ea8ce1acf718ec730b30b2f9fb795b30f706247a74a76558170aec5985d43'
